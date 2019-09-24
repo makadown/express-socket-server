c5bdf221c3b7b71461e6cdfab8431cec6c4fa561
Makadown <maseflores79@gmail.com>
2019-09-23 21:04:56 -0500

    Comenzando a meter httpServer y socketIO

--- dist/classes/server.js
+++ dist/classes/server.js
@@ -5,13 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../global/environment");
+const socket_io_1 = __importDefault(require("socket.io"));
+const http_1 = __importDefault(require("http"));
 class Server {
     constructor() {
         this.app = express_1.default();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = socket_io_1.default(this.httpServer);
+        this.escucharSockets();
+    }
+    escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+        });
     }
     start(callback) {
-        this.app.listen(this.port, callback());
+        // this.app.listen( this.port, callback() );
+        this.httpServer.listen(this.port, callback());
     }
 }
 exports.default = Server;
