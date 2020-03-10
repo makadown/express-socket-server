af6994f54e2e44611953d2321617e553752416e3
makadown <maseflores79@gmail.com>
2020-03-09 20:21:07 -0600

    Crear clase backend para controlar la grafica

---
+++ classes/grafica.ts
@@ -0,0 +1,25 @@
+export class graficaData {
+
+    private meses = ['enero', 'febrero', 'marzo', 'abril'];
+    private valores: number[] = [1,2,3,4];
+
+    constructor() {
+
+    }
+
+    getDataGrafica() {
+        return [ { data: this.valores, label: 'Ventas' }];
+    }
+
+    incrementarValor(mes: string, valor: number) {
+        mes = mes.toLowerCase().trim();
+        for(let i in this.meses) {
+            if ( this.meses[i] === mes ) {
+                this.valores[i] += valor;
+            }
+        }
+
+        return this.getDataGrafica();
+    }
+
+}
